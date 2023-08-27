f5ca6e7d8d25d49e28f48fca668b5f593a716bbc
Dragan <milosevicdragan002@gmail.com>
2023-08-27 08:39:17 +0200

    Add style for single product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,35 @@
 @extends('header')
 @include('partials._hero')
-@include('partials._search')
 @section('content')
-
-<h1>{{$product->name}}</h1>
+<div class="container">
+<div class="row">
+    <div class="col-lg-6">
+        <img  src="{{ asset('images/no-image.png') }}" class="image-fluid" style = "height:330px; object-fit:cover;width:450px"  >
+    </div>
+    <div class="col-lg-6">
+        <h2>{{$product['name']}}</h2>
+        <p>Size:{{$product['size']}}</p>
+        <p>Price: ${{$product['price']}}</p>
+        <form action="" method = "post">
+            <input type="number" name="quantity" required style="background-color: #f1f1f1; border: 1px solid #ccc; padding: 5px;">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Add to Cart</button>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Do you want to continue shopping or go to the cart?</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal" name = "cart" >Go to cart</button>
+                        <button type="submit" class="btn btn-primary" name = "continue">Continue</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        </form>
+    </div>
+</div>
+</div>
 @endsection
