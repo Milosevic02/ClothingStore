b855c72806eae1c13d836ed8d4c0d96747ad1754
Dragan <milosevicdragan002@gmail.com>
2023-08-27 10:04:27 +0200

    edit search bar

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,42 +1,9 @@
-<link
-rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-crossorigin="anonymous"
-referrerpolicy="no-referrer"
-/>
-<script src="https://cdn.tailwindcss.com"></script>
-<script>
-tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                laravel: "#ef3b2d",
-            },
-        },
-    },
-};
-</script>
 <form action="">
-    <div class="relative border-2 border-gray-100 m-4 rounded-lg">
-        <div class="absolute top-4 left-3">
-            <i
-                class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
-            ></i>
-        </div>
-        <input
-            type="text"
-            name="search"
-            class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Search Laravel Gigs..."
-        />
-        <div class="absolute top-2 right-2">
-            <button
-                type="submit"
-                class="h-10 w-20 text-white rounded-lg bg-purple-500 hover:bg-purple-600"
-            >
-                Search
-            </button>
+    <div class="container">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search Product" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-primary" type="button" id="button-addon2">Search</button>
         </div>
     </div>
+
 </form>
