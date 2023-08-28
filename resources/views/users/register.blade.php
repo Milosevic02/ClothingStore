6a1ff7c240d6f9ed190d7d9c012655a2ce982270
Dragan <milosevicdragan002@gmail.com>
2023-08-28 08:44:38 +0200

    Preserve the previous value in case of a failed registration

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -5,7 +5,7 @@
         @csrf
         <div class="form-group mb-3">
             <label for="name">Full Name</label>
-            <input type="text" class="form-control" id = "name" name = "name" required>
+            <input type="text" class="form-control" id = "name" name = "name" value="{{old('name')}}" >
             @error('name')
                 <p class="text-danger mt-1">{{$message}}</p>    
             @enderror
@@ -13,7 +13,7 @@
 
         <div class="form-group mb-3">
             <label for="email">Email address</label>
-            <input type="text" class = "form-control" id = "email" name = "email" required>
+            <input type="text" class = "form-control" id = "email" name = "email" value="{{old('email')}}" >
             @error('email')
                 <p class="text-danger mt-1">{{$message}}</p>    
             @enderror
@@ -21,14 +21,14 @@
 
         <div class="form-group mb-3">
             <label for="password">Password</label>
-            <input type="password" class = "form-control" id = "password" name = "password" required>
+            <input type="password" class = "form-control" id = "password" name = "password" value="{{old('password')}}">
             @error('password')
                 <p class="text-danger mt-1">{{$message}}</p>    
             @enderror
         </div>
         <div class="form-group mb-3">
             <label for="password">Confirm Password</label>
-            <input type="password" class = "form-control" id = "password" name = "password_confirmation" required>
+            <input type="password" class = "form-control" id = "password" name = "password_confirmation" value="{{old('password_confirmation')}}" >
             @error('password_confirmation')
                 <p class="text-danger mt-1">{{$message}}</p>    
             @enderror
