f0560825738b45edd75ada8b1ca5a5c3d15bf66c
Dragan <milosevicdragan002@gmail.com>
2023-08-30 11:14:58 +0200

    Modified the 'View Product' page to provide admin users with the options to 'Edit' and 'Delete' products instead of 'Add to Cart'.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -20,25 +20,41 @@
             <p style="font-weight: bold;">Size: {{$product['size']}}</p>
 
         </h5>
-        <form action="" method = "post">
-            <input type="number" name="quantity" required style="background-color: #f1f1f1; border: 1px solid #ccc; padding: 5px;">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Add to Cart</button>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Do you want to continue shopping or go to the cart?</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal" name = "cart" >Go to cart</button>
-                        <button type="submit" class="btn btn-primary" name = "continue">Continue</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
-        </form>
+
+        @auth
+            @can('admin')
+            <a href="/products/edit" class="btn btn-warning btn-lg" style="margin-right: 10px">
+                <i class="bi bi-pencil"></i> Edit
+            </a>
+            <a href="/products/delete" class="btn btn-danger btn-lg">
+                <i class="bi bi-trash"></i> Delete
+            </a>
+            
+            @else
+                <form action="" method = "post">
+                    @csrf
+                    <input type="number" name="quantity" required style="background-color: #f1f1f1; border: 1px solid #ccc; padding: 5px;">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Add to Cart</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Do you want to continue shopping or go to the cart?</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal" name = "cart" >Go to cart</button>
+                                <button type="submit" class="btn btn-primary" name = "continue">Continue</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                </form>
+            @endcan
+
+        @endauth
+
     </div>
 </div>
 </div>
