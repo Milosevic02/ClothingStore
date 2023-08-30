431f86f8d69003724a4d5ce23346ad52c3727e23
Dragan <milosevicdragan002@gmail.com>
2023-08-30 11:13:54 +0200

    include bootstrap icon

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,6 +7,7 @@
 
     <title>Mens Boutique</title>
     <script src="//unpkg.com/alpinejs" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
@@ -58,9 +59,12 @@
                         <li class="nav-item">
                             <form action="/logout" method="POST">
                                 @csrf
-                                <button class="btn btn-danger" type="submit">Logout</button>
+                                <button class="btn btn-danger" type="submit">
+                                    <i class="bi bi-box-arrow-right"></i> Logout
+                                </button>
                             </form>
                         </li>
+                        
                     @else
                     <li class="nav-item">
                         <a href="/register" class="nav-link">
